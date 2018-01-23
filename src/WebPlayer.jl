e3e0ab4f70c0a449544581825a03d1037054b98c
SimonDanisch <sdanisch@gmail.com>
2018-01-23 22:36:04 +0100

    base64 encode and inline video data

--- src/WebPlayer.jl
+++ src/WebPlayer.jl
@@ -31,29 +31,31 @@ function set_time(name, nvideos, val, nframes){
 }
 """
 
-function video_player(video, name = "test", width)
-    dir = joinpath(pwd(), "assets")
-    isdir(dir) || mkdir("assets")
-    xdim, ydim, nframes = size(video)
-    frame = Matrix{RGB{N0f8}}(xdim, ydim)
-    path = joinpath(dir, "$name.mkv")
-    io, process = open(`ffmpeg -loglevel quiet -f rawvideo -pixel_format rgb24 -r 24 -s:v $(xdim)x$(ydim) -i pipe:0 -vf vflip -y $path`, "w")
-    for i = 1:nframes
-        frame = RGB{N0f8}.(Gray.(view(video, :, size(video, 2):-1:1, i)))
-        write(io, frame)
+function video_player(video, name, width)
+    mktempdir() do dir
+        xdim, ydim, nframes = size(video)
+        frame = Matrix{RGB{N0f8}}(xdim, ydim)
+        path = joinpath(dir, "$name.mkv")
+        io, process = open(`ffmpeg -loglevel quiet -f rawvideo -pixel_format rgb24 -r 24 -s:v $(xdim)x$(ydim) -i pipe:0 -vf vflip -y $path`, "w")
+        for i = 1:nframes
+            frame = RGB{N0f8}.(Gray.(view(video, :, size(video, 2):-1:1, i)))
+            write(io, frame)
+        end
+        close(io)
+        sleep(1)
+        mp4path = joinpath(dir, "$(name).mp4")
+        run(`ffmpeg -loglevel quiet -y -i $(path) -c:v libx264 -preset slow -crf 22 -pix_fmt yuv420p -c:a libvo_aacenc -b:a 128k -y $(mp4path)`)
+        sleep(2)
+        println(length(read(mp4path)))
+        dom"video"(
+            dom"source"(attributes = Dict(
+                :src => string("data:video/mp4;base64,", base64encode(read(mp4path))),
+                :type => "video/mp4",
+            )),
+            id = name,
+            attributes = Dict(:loop => "", :width => "100%")
+        )
     end
-    close(io)
-    sleep(1)
-    mp4path = joinpath(dir, "$(name).mp4")
-    rm(mp4path, force = true)
-    run(`ffmpeg -loglevel quiet -y -i $(path) -c:v libx264 -preset slow -crf 22 -pix_fmt yuv420p -c:a libvo_aacenc -b:a 128k -y $(mp4path)`)
-    dom"video"(
-        dom"source"(attributes = Dict(
-            :src => "files/assets/$(name).mp4", :type => "video/mp4",
-        )),
-        id = name,
-        attributes = Dict(:loop => "", :width = "100%")
-    )
 end
 
 package_dir() = joinpath(@__DIR__, "..")
@@ -90,7 +92,7 @@ function playvideo(videos::Vector{Array{T, 3}}, names = ["video $i" for i = 1:le
     onjs(timestep, @js function (val)
         $(set_time)($unique_name, $nvideos, val, $(nframes))
     end)
-    video_players = w.(video_player.(videos, [string(unique_name, i) for i = 1:nvideos]))
+    video_players = w.(video_player.(videos, [string(unique_name, i) for i = 1:nvideos], width))
     vbox(hbox(w(button), s), hbox(videobox.(video_players, names, width)...))
 end
 
